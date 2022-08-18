fc910af641a06574266a50ff426d8da4495b6795
Abdullah Hassan <abdullahzen25@gmail.com>
2022-08-18 17:09:58 +0200

    controllers generated

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,10 @@
+class BookmarksController < ApplicationController
+
+  def create
+
+  end
+
+  def new
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :list, only: [:index, :show, :new, :create, :destroy] do
+    resources :bookmark, only: [:create, :new]
+  end
 end
